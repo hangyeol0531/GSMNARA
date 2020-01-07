c14f45a7dd9c0bb0f00e64e72621b6bf1b255b83
hangyeol0531 <41174265+hangyeol0531@users.noreply.github.com>
2020-01-07 23:45:45 +0900

    end

--- gsmnara/routes/index.js
+++ gsmnara/routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended : true}));
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -10,11 +14,20 @@ router.get('/', function(req, res) {
 router.get('/register', function(req, res){
   console.log("회원가입 창 접속");
   res.render('register')
-  
+
 })
 
 router.get('/main', function(req, res){
   console.log("메인 창 접속");
   res.render('main')
 })
+
+router.post('/getinformation', function(req, res){
+  console.log("회원가입 정보삽입을 할게요.");
+  var student_id = req.body.student_id;
+  console.log(student_id);
+  console.log(student_id);
+  res.render('main');
+});
+
 module.exports = router;
